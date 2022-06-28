e9a6e1e39edfa87b4f370065bf0d88053a767ed5
mukhanovofox <mukhanovfox@gmail.com>
2022-06-28 17:43:31 +0600

    defaul config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
-module.exports = {
-    devServer: {
-        proxy: 'http://metagate.city',
-    }
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+// module.exports = {
+//     devServer: {
+//         proxy: 'http://metagate.city',
+//     }
+// }
